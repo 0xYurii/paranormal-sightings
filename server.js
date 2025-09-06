d9560f9e98d7fb68bbd8aeeac36979a05f1e6a40
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-06 11:30:10 +0100

    import path

--- server.js
+++ server.js
@@ -1,29 +1,19 @@
 import http from "node:http"
-
+import path from "node:path"
+const __dirname=import.meta.dirname
 
 const PORT =8000
+console.log(__dirname)
 
 const server=http.createServer((req,res)=>{
-    res.statusCode=200
-    res.end(`<html><h1>The server is working</h1></html>`)
+
+    const pathToResources=path.join(__dirname,"public","index.html")
+    console.log(pathToResources)
+    res.writeHead(200,{"Content-Type":"text/html"})
+    
+    res.end(`<html><h1>The server is working!!</h1></html>`)
 })
 
 
 server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
-
-/*
-
-Challenge: 
-
-1.Set up a server that serves the string 
-    '<html><h1>The server is working</h1></html>'.
-    
-    What should the content type be? 
-    What status code should you send?
-
-2. Listen on port 8000 and log a connection message to the console.
-
-3. Open the browser to see your first served HTML.
-
-*/
 
