b1227f7d10eed84bba5f870551dacc6e95c1db69
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-06 18:02:51 +0100

    serve file from public dir

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const __dirname = import.meta.dirname
 
 const server = http.createServer(async(req, res) => {
     
-    await serveStatic(__dirname,res)
+    await serveStatic(__dirname,res,req)
 })
 
 server.listen(PORT, ()=> console.log(`Connected on port: ${PORT}`))
--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -1,14 +1,21 @@
 import path from 'node:path'
 import fs from 'node:fs/promises'
 import sendResponse from './sendResponse.js'
+import getContentType from './getContentType.js'
 
-export default async function serveStatic(baseDir,res) {
-  
-  const filePath = path.join(baseDir, 'public', 'index.html')
-  
+export default async function serveStatic(baseDir,res,req) {
+  const publicDir=path.join(baseDir, 'public', )
+  const filePath = path.join(publicDir,req.url==="/"?
+    "index.html":
+    req.url
+  )
+  const ext=path.extname(filePath)
+  const content_type=getContentType(ext)
+
+
   try{
     const content=await fs.readFile(filePath)
-    sendResponse(res,200,content,'text/html')
+    sendResponse(res,200,content,content_type)
   }catch(error){
     console.log("Failed read the file ",error.message)
     sendResponse(res,500,"Failed read the file ",'text/plain')
