4ff67dc71a6911e12ad6b755f6f5229ab8ce6436
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-08 22:13:55 +0100

    add sightingadd as EventEmitter

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -4,7 +4,7 @@ import sendResponse from "../utils/sendResponse.js"
 import { parseJSONBody } from '../utils/parseJSONBody.js'
 import { addNewSighting } from '../utils/addNewSighting.js'
 import { sanitizeInput } from '../utils/sanitizeInput.js'
-
+import { sightingEvents } from "../events/sightingEvent.js"
 export default async function handleGet(res){
    const content=await (getData())
    const data= JSON.stringify(content)
@@ -13,11 +13,17 @@ export default async function handleGet(res){
 }
 
 
+
 export async function handlePost(req,res){
    try{
       const parsedBody=await parseJSONBody(req)
       const sanitizedBody = sanitizeInput(parsedBody)
       addNewSighting(sanitizedBody)
+      sightingEvents.emit('sighting-added',sanitizedBody)
+
+
+
+
       sendResponse(res,201,JSON.stringify(sanitizedBody),'application/json')
    }catch(err){
       console.log(err)
